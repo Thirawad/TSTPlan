1f4c9e069eb3b6f46cf029e4061fd353a1326377
Thirawad <thirawad2099@gmil.com>
2018-12-12 19:09:14 +0700

    commit

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
 
 
 const Navbar = () => {
   return (
-    <nav className="nav-wrapper grey darken-3">
+    <nav className="nav-wrapper light-green darken-4">
       <div className="container">
         <Link to='/' className="brand-logo">TST Plan</Link>
         <SignedInLinks />
+        <SignedOutLinks />
       </div>
     </nav> 
   )
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -6,7 +6,7 @@ const SignedInLinks = () => {
     <ul className="right">
       <li><NavLink to='/'>New Project</NavLink></li>
       <li><NavLink to='/'>Log Out</NavLink></li>
-      <li><NavLink to='/' className='btn btn-floating pink lighten-1'>NN</NavLink></li>
+      <li><NavLink to='/' className='btn btn-floating orange darken-4'>NN</NavLink></li>
     </ul>
   )
 }
